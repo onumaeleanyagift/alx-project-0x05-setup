7f1fb45cc3da12413e365782e66bcb7ac93e0cdc
Eleanya Onuma <onumaeleanyagift@gmail.com>
2025-08-16 17:35:32 +0100

    feat: enhance Home page with generated images history and grid display

--- alx-project-0x11/pages/index.tsx
+++ alx-project-0x11/pages/index.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleGenerateImage = async () => {
@@ -26,14 +27,17 @@ const Home: React.FC = () => {
 
     const data = await resp.json();
     setIsLoading(false);
+    setImageUrl(data?.message);
+    setGeneratedImages((prev) => [
+      ...prev,
+      { imageUrl: data?.message, prompt },
+    ]);
   };
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-2 text-black">
-          Image Generation App
-        </h1>
+        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
         <p className="text-lg text-gray-700 mb-4">
           Generate stunning images based on your prompts!
         </p>
@@ -44,7 +48,7 @@ const Home: React.FC = () => {
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
-            className="w-full p-3 border border-gray-300 rounded-lg mb-4 text-black"
+            className="w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
           <button
             onClick={handleGenerateImage}
@@ -62,6 +66,25 @@ const Home: React.FC = () => {
           />
         )}
       </div>
+      {generatedImages.length ? (
+        <div className="">
+          <h3 className="text-xl text-center mb-4">Generated Images</h3>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 border max-w-full md:max-w-[1100px] p-2 overflow-y-scroll h-96">
+            {generatedImages?.map(({ imageUrl, prompt }: ImageProps, index) => (
+              <ImageCard
+                action={() => setImageUrl(imageUrl)}
+                imageUrl={imageUrl}
+                prompt={prompt}
+                key={index}
+                width="w-full"
+                height="h-40"
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
